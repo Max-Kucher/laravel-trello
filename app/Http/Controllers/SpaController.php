eded19f6dd747b251a31c08028106546c7668c1d
Max-Kucher <mkuchirov@gmail.com>
2023-02-10 21:32:56 +0200

    [n] Добавлена дефолтная вьюха

--- app/Http/Controllers/SpaController.php
+++ app/Http/Controllers/SpaController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class SpaController extends Controller
 {
-    //
+    public function index(Request $req) : View
+    {
+        return view('index');
+    }
 }
