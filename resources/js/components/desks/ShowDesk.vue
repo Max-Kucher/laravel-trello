0129889b35ab66960a70a23549c9ebb79287da7d
Max-Kucher <mkuchirov@gmail.com>
2023-02-12 21:32:35 +0200

    [n] Валидация форма с фронта.

--- resources/js/components/desks/ShowDesk.vue
+++ resources/js/components/desks/ShowDesk.vue
@@ -1,20 +1,37 @@
 <template>
     <div class="container">
         <div class="row">
-            <h1 class="col-12 mt-3 mb-5">{{ desk.desk_name || "Загрузка..." }}</h1>
+            <h1 class="col-12 mt-3 mb-5">{{ loading ? 'Загрузка...' : desk.desk_name }}</h1>
         </div>
 
-        <form method="post" @submit.stop.prevent="submitClick">
+        <form method="post" @submit.stop.prevent="submitClick" class="needs-validation" novalidate>
             <div class="mb-3">
                 <label for="desk_name" class="form-label">Название доски</label>
                 <input name="desk_name"
                        id="desk_name"
                        type="text"
                        class="form-control"
+                       :class="{ 'is-invalid': v$.desk.desk_name.$error, 'is-valid': !v$.desk.desk_name.$error }"
                        v-model="desk.desk_name"
                        placeholder="Название доски"
                        aria-label="Название доски"
+                       required
+                       @keyup.stop.prevent="v$.desk.desk_name.$touch"
+                       @blur="validate"
                 >
+
+                <div class="valid-feedback">
+                    Выглядит здорово!
+                </div>
+                <div class="invalid-feedback" v-if="v$.desk.desk_name.required.$invalid">
+                    Вы должны заполнить название доски.
+                </div>
+                <div class="invalid-feedback" v-if="v$.desk.desk_name.minLength.$invalid">
+                    Длинна названия доски должна быть больше {{ v$.desk.desk_name.minLength.$params.min }} букв
+                </div>
+                <div class="invalid-feedback" v-if="v$.desk.desk_name.maxLength.$invalid">
+                    Длинна названия доски не должна быть больше {{ v$.desk.desk_name.maxLength.$params.max }} букв
+                </div>
             </div>
 
             <div class="mb-3">
@@ -25,6 +42,8 @@
                           v-model="desk.desk_description"
                           rows="3"
                 >{{ desk.desk_description }}</textarea>
+
+                <!--                          @keyup.stop.prevent="validate"-->
             </div>
 
             <div class="mb-3">
@@ -37,14 +56,23 @@
 </template>
 
 <script>
+import { useVuelidate } from '@vuelidate/core';
+import { required, maxLength, minLength } from '@vuelidate/validators';
+
 export default {
     props: [
         'deskId',
     ],
+    setup() {
+        return {
+            v$: useVuelidate()
+        }
+    },
     data() {
         return {
             desk: { },
             error: false,
+            loading: true,
         }
     },
     mounted() {
@@ -58,10 +86,16 @@ export default {
                 console.log(err);
 
                 this.error = true;
-        });
+        }).finally(() => { this.loading = false });
     },
     methods: {
-        submitClick: function() {
+        async submitClick() {
+            const validation_result = await this.v$.$validate();
+
+            if (!validation_result) {
+                return false;
+            }
+
             let desk_data = JSON.parse(JSON.stringify( this.desk ));
 
             axios.patch('/api/v1/desks/' + this.deskId, desk_data)
@@ -69,6 +103,15 @@ export default {
                 console.log(response);
             });
         },
+    },
+    validations: {
+        desk: {
+            desk_name: {
+                required,
+                maxLength: maxLength(255),
+                minLength: minLength(3),
+            },
+        }
     },
 }
 </script>
